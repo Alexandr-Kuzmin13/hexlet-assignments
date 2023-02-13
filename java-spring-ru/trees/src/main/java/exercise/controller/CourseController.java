c5ac97f9ae9207ab5c46e35d6d03bc7084762dfb
@hexlet/cli <support@hexlet.io>
2023-02-13 17:04:24 +0300

    submit java-spring-ru/trees

--- java-spring-ru/trees/src/main/java/exercise/controller/CourseController.java
+++ java-spring-ru/trees/src/main/java/exercise/controller/CourseController.java
@@ -1,6 +1,5 @@
 package exercise.controller;
 
-import com.sun.xml.bind.v2.model.core.ID;
 import exercise.model.Course;
 import exercise.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -32,7 +30,7 @@ public class CourseController {
     }
 
     // BEGIN
-    @GetMapping(path = "/{id}/previous/")
+    @GetMapping(path = "/{id}/previous")
     public Iterable<Course> getCourseList(@PathVariable long id) {
         Course course = courseRepository.findById(id);
         List<Long> ids = Arrays.stream(course.getPath().split("\\."))
