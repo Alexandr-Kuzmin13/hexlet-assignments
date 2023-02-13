26d72b5b8a4dafec4fdf5de3fe3b22e1180205ab
@hexlet/cli <support@hexlet.io>
2023-02-13 12:14:07 +0300

    submit java-spring-ru/relations

--- java-spring-ru/relations/src/main/java/exercise/model/Category.java
+++ java-spring-ru/relations/src/main/java/exercise/model/Category.java
@@ -2,7 +2,6 @@ package exercise.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
